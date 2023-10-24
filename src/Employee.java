fdf2326c6c8fe4c2ca4c00977d1a68ed52cc8b73
n3mochaken <ns.mixa@gmail.com>
2023-10-24 12:01:14 +0300

    done

---
+++ src/Employee.java
@@ -0,0 +1,41 @@
+public class Employee {
+    private static int count;
+    private int id;
+    private String fullName;
+
+    private int department;
+    private int salary;
+
+    public Employee(String fullName, int salary) {
+        this.fullName = fullName;
+        this.salary = salary;
+        count++;
+        id = count;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSalary(int i) {
+        this.salary = salary;
+
+    }
+
+    public void setDepartment(int i) {
+        this.department = department;
+
+    }
+    @Override
+    public String toString() {
+        return "ID: " + id + ", ФИО: " + fullName+ ", Отдел: " + department + ", Зарплата: " + salary;
+    }
+}
